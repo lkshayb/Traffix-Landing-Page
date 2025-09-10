16dd7bed01da80af6db48b2f7b86717c8fbb4af4
Lakshay Bhatia <b.lakshay.aspect@gmail.com>
2025-09-10 18:39:19 +0530

    Migrate code to Vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/navbar.jsx";
 import Hero from "./components/hero.jsx";
 import Features from "./components/features.jsx";
